a56c63e2fee4ce649e6c230a017341a7f2a8bc4d
Kantum <kentin@ymail.com>
2018-07-13 19:44:46 +0200

    push du jour

---
+++ index.js
@@ -0,0 +1,30 @@
+const bigInt = require("big-integer");
+const Web3 = require('web3');
+const fs = require('fs');
+var web3 = new Web3(Web3.givenProvider || new Web3.providers.HttpProvider('https://mainnet.infura.io/yqewpKnGbyqWrSNtiZzK'));
+
+checkAddressBalance = (privateIndex, callback) => {
+    var privateKey = new Buffer.from(privateIndex.toString(16).padStart(64, '0'), 'hex');
+    var publicAddress = web3.eth.accounts.privateKeyToAccount(privateKey).address;
+    web3.eth.getBalance(publicAddress)
+        .then((balance)=>callback(privateIndex, balance/1000000000000000000),
+                (err)=>console.error('err on ' + publicAddress)//checkAddressBalance(privateIndex, callback)
+        );
+};
+
+showBalance = (privateIndex, balance) => {
+    fs.writeFileSync('state.txt', privateIndex.toString(16));
+    if (balance)
+        fs.appendFileSync('output.txt', privateIndex.toString(16).padStart(64, '0') + " => " + balance + "\n");
+};
+
+(async ()=>{
+    var previousIndex = (fs.readFileSync('state.txt').toString() || "1").padStart(64, '0');
+    
+    var privateIndex = bigInt(previousIndex, 16);
+    console.log("Current scan index at " + previousIndex);
+    for (let i = 0; i < 1000; i++) {
+        checkAddressBalance(privateIndex, showBalance);
+        privateIndex = privateIndex.add(1);
+    }
+})();
